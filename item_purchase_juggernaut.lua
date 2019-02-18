563708011404ad0909f814c1dc9200d986e9fdce
Jacob <43463199+j6i@users.noreply.github.com>
2019-02-17 19:02:49 -0800

    item builds for main heros

---
+++ item_purchase_juggernaut.lua
@@ -0,0 +1,34 @@
+local module = require(GetScriptDirectory().."/helpers")
+
+local Items = {
+	----poor man's shield----
+	"item_stout_shield",
+	"item_slippers",
+	"item_slippers",
+
+	----powerthreads----
+	"item_belt_of_strength",
+	"item_boots",
+	"item_gloves",
+
+
+	----battlefury----
+	"item_quelling_blade",
+	"item_ring_of_health",
+	"item_void_stone",
+	"item_demon_edge",
+	"item_recipe_bfury",
+
+	----Desolator----
+	"item_mithril_hammer",
+	"item_mithril_hammer",
+	"item_blight_stone",
+
+
+	----Vladmir's Offering----
+	"item_sobi_mask",
+	"item_ring_of_protection",
+	"item_lifesteal",
+	"item_recipe_vladmir"
+}
+return Items
